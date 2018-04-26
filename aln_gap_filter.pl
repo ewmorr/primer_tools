5f67de639a1ccb1ceafb506a546a893b8d215298
Eric Morrison <emorrison40@gmail.com>
2018-04-26 14:08:59 -0700

    mods to aln_gap_filter.pl

--- aln_gap_filter.pl
+++ aln_gap_filter.pl
@@ -20,7 +20,7 @@ if(defined($ARGV[1]) == 0)
 my $in = $ARGV[0];
 my $dir = $ARGV[1];
 my $minLen = $ARGV[2];
-system "mkdir $dir";
+#system "mkdir $dir";
 
 open(IN, "$in") || die "Can't open amino acid sequence file.\n";
 $in =~ /.*\/(.*)/;
@@ -74,18 +74,63 @@ for(my $i = 0; $i<length $bare_aln[0]; $i++)
 			}
 		}
 	}
+
+#Check for no gap alns
+if(scalar(@gap_pos) == 0)
+	{
+	open(OUT, ">$dir/$fileName".".full") || die "Can't open output\n";
+	foreach my $key (keys %aln)
+		{	
+		print OUT ">", $key, "_pos_0_", length($aln{$key})-1, "\n", $aln{$key}, "\n";
+		}
+	print "no gaps in $fileName\n";
+	exit;
+	}
+
+my $fileCount = 0;
+
+#Check for alns with only a single gap which are not properly handled by the main loop
+if(scalar(@gap_pos) == 1)
+	{
+	if($gap_pos[0] > $minLen)
+		{
+		open(OUT, ">$dir/$fileName".".single.$fileCount") || die "Can't open output\n";
+		$fileCount++;
+		foreach my $key (keys %aln)
+			{
+			print OUT ">", $key, "_pos_0_", $gap_pos[0]-1, "\n", substr($aln{$key}, 0, $gap_pos[0]), "\n";
+			}
+		}
+	if(length($bare_aln[0])-$gap_pos[0] > $minLen)
+		{
+		open(OUT, ">$dir/$fileName".".single.$fileCount") || die "Can't open output\n";
+		$fileCount++;
+		foreach my $key (keys %aln)
+			{
+			print OUT ">", $key, "pos_", $gap_pos[-1]+1, "_", length($bare_aln[0])-1, "\n", substr($aln{$key}, $gap_pos[-1]+1, length($bare_aln[0])-$gap_pos[-1]-1), "\n";
+			}
+		}
+	print "One gap at $gap_pos[0] in $fileName\n";
+	exit;
+	}
+
+#cut seqs
+print "Gap positions in $fileName\n";
+
+#need to start with zero offset if it is not a gap. If there is a single gap at the first position this should find that
 if($gap_pos[0] != 0)
 	{
-	unshift(@gap_pos, 0);
+	if($gap_pos[0] > $minLen)
+		{
+		open(OUT, ">$dir/$fileName".".$fileCount") || die "Can't open output\n";
+		$fileCount++;
+		foreach my $key (keys %aln)
+			{
+			print OUT ">", $key, "_pos_0_", $gap_pos[0]-1, "\n", substr($aln{$key}, 0, $gap_pos[0]), "\n";
+			}
+		}
 	}
-if($gap_pos[-1] != length($bare_aln[0])-1)
-	{
-	push(@gap_pos, length($bare_aln[0]));
-	}
-	
-#cut seqs
-my $fileCount = 0;
-print "Gap positions in $in\n";
+
 for(my $i = 1;$i<@gap_pos;$i++)
 	{
 	print $gap_pos[$i], " ";
@@ -96,11 +141,25 @@ for(my $i = 1;$i<@gap_pos;$i++)
 	open(OUT, ">$dir/$fileName".".$fileCount") || die "Can't open output\n";
 	$fileCount++;
 	foreach my $key (keys %aln)
-		{
-		print OUT ">", $key, "\n", substr($aln{$key}, $gap_pos[$i-1], $gap_pos[$i]-$gap_pos[$i-1]), "\n";
+		{	
+		print OUT ">", $key, "_pos_", $gap_pos[$i-1]+1, "_", $gap_pos[$i]-1, "\n", substr($aln{$key}, $gap_pos[$i-1]+1, $gap_pos[$i]-$gap_pos[$i-1]-1), "\n";
 		}
 	}
 print "\n";
+
+#need to end with final offset if this is not a gap. If there is a single gap at the last position this should find that
+if($gap_pos[-1] != length($bare_aln[0])-1)
+	{
+	if(length($bare_aln[0])-$gap_pos[-1] > $minLen)
+		{
+		open(OUT, ">$dir/$fileName".".$fileCount") || die "Can't open output\n";
+		$fileCount++;
+		foreach my $key (keys %aln)
+			{
+			print OUT ">", $key, "pos_", $gap_pos[-1]+1, "_", length($bare_aln[0])-1, "\n", substr($aln{$key}, $gap_pos[-1]+1, length($bare_aln[0])-$gap_pos[-1]-1), "\n";
+			}
+		}
+	}
 
 
 
